bb6c0d77b1c60003cb266eb549af9f110a853cc4
Jorgen Birkler <jorgen@birkler.se>
2026-01-12 17:22:33 -0800

    Update jura.h

--- components/jura/jura.h
+++ components/jura/jura.h
@@ -11,6 +11,10 @@
 namespace esphome {
 namespace jura {
 
+static uint8_t bitRead(uint8_t val, uint8_t bita) {
+  return (val & (1<<bita) != 0) ? 1 : 0;
+}
+
 class Jura : public PollingComponent, public uart::UARTDevice {
  public:
   void set_model(const std::string &m) { model_ = m; }
